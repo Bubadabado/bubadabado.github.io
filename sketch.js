baa6379eddb7fed5f84031a61ef18b6720c9d212
Bubadabado <adam@adamrobertson.us>
2017-03-26 19:19:33 -0500

    random effect

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
+var r, g, b;
 function setup() {
   createCanvas(640, 480);
+  r = random(255);
+  g = random(255);
+  b = random(255);
+
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    fill(r, g, b, 127);
+    r = random(255);
+    g = random(255);
+    b = random(255);
+
   } else {
     fill(255);
   }
